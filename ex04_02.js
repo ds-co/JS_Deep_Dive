40d916d6db16dfae33a1041f2fa7f0d80868746a
ds-co <91958912+ds-co@users.noreply.github.com>
2022-03-01 14:37:56 +0900

    Add files via upload

---
+++ ex04_02.js
@@ -0,0 +1,11 @@
+// 변수는 하나의 값을 저장하기 위한 수단이다.
+var userId = 1;
+var userName = 'OH';
+
+// 객체나 배열 같은 자료구조를 사용하면 여러 개의 값을 하나로 그룹화해서 하나의 값처럼 사용할 수 있다.
+var user = { id: 2026049, name: 'OH'};
+
+var users = [
+    {id: 7, name:'OH'},
+    {id: 8, name:'YU'}
+];
---
+++ ex04_09.js
@@ -0,0 +1,5 @@
+console.log(score); // undefined
+
+var score = 80;
+
+console.log(score);
